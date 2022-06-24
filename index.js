fd290f2556728612ffb737aa7f23014466413e50
e-iroro <eruiroro18@gmail.com>
2022-06-24 03:29:31 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const eru ={
+    name: 'Eruoghene Iroro',
+    height: "5'9",
+    country: 'Abuja, Nigeria',
+}
+
+alert(
+    `Hello everyone! My name is ${eru.name},\nI am ${eru.height} \nand I am from ${eru.country}.`
+);
